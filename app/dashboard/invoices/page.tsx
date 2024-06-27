d3af183bf4ef224aa13dec369f1c25f7ee78263c
Sajjad Rafiee <srfie7146@gmail.com>
2024-06-27 15:58:03 -0400

    I finished Next.js course for beginning.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import {lusitana} from '@/app/ui/fonts';
 import {InvoicesTableSkeleton} from '@/app/ui/skeletons';
 import {Suspense} from 'react';
 import {fetchInvoicesPages} from "@/app/lib/data";
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
 
 export default async function Page({searchParams,}: { searchParams?: { query?: string; page?: string; } }) {
     const query = searchParams?.query || '';
@@ -24,7 +29,7 @@ export default async function Page({searchParams,}: { searchParams?: { query?: s
                 <Table query={query} currentPage={currentPage}/>
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                 <Pagination totalPages={totalPages} />
+                <Pagination totalPages={totalPages}/>
             </div>
         </div>
     );
